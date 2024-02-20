e883b63da29c4aa1ebfd166adf397843ad633474
Monicaasensio <Monicaasensio@yahoo.com>
2024-02-20 14:27:50 -0500

    unit tests

--- src/__test__/Footer.test.jsx
+++ src/__test__/Footer.test.jsx
@@ -1,10 +1,12 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import Footer from '../components/Footer';
 
 test("Example 1 renders successfully", () => {
-    render(<Footer/>);
+    render(<Router><Footer/></Router>);
 
-    const element = screen.getByText(/Links/i);
+    const element = screen.getByText(/Home/i);
 
     expect(element).toBeInTheDocument();
 })
